10b1328a19cf05da83d8d1c263fb2a89eff0c24f
lints123 <352930759@qq.com>
2018-11-07 15:33:53 +0800

    注释

--- src/main/java/com/lints/tools/rabbitmq/HelloSender1.java
+++ src/main/java/com/lints/tools/rabbitmq/HelloSender1.java
@@ -6,19 +6,22 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+/**
+ * 单个生产者消费者
+ */
+
 @Component
 public class HelloSender1 {
 
     @Autowired
     private AmqpTemplate rabbitTemplate;
+
     // 最简单的单个生产者和消费者
     public void send() {
-        for(int i =0;i<10;i++) {
-            String sendMsg = "hello1 " + new Date();
-            System.out.println("Sender1 : " + sendMsg+"____"+i);
-            // 发送消息到队列中，使用默认路由器，指明队列名称
-            this.rabbitTemplate.convertAndSend("helloQueue", sendMsg);
-        }
+        String sendMsg = "hello1 " + new Date();
+        System.out.println("Sender1 : " + sendMsg);
+        // 发送消息到队列中，使用默认路由器，指明队列名称
+        this.rabbitTemplate.convertAndSend("helloQueue", sendMsg);
     }
 
 }
